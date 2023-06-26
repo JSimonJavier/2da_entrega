cd15e5dfffd3bc05cddef4946df81e9da3ac824c
Javier Simon <simonjavier.fullstack@gmail.com>
2023-06-26 20:01:44 -0300

    se agrego login por formulario y otros detalles

--- src/app.js
+++ src/app.js
@@ -9,11 +9,17 @@ import homeRoutes from "./routes/mongo/homeRoutes.js";
 import productRoutes from "./routes/mongo/productRoutes.js";
 import cartRoutes from "./routes/mongo/cartRoutes.js";
 import chatRoutes from "./routes/mongo/chatRoutes.js";
+import authRoutes from "./routes/mongo/authRoutes.js";
 import websockets from "./websockets/websockets.js";
 import exphbs from "express-handlebars";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { connectMongoDB } from "./config/configMongoDB.js";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { passport } from "./auth/passport-local.js";
+import flash from "connect-flash";
 
 /** ★━━━━━━━━━━━★ variables ★━━━━━━━━━━━★ */
 
@@ -21,7 +27,7 @@ const app = express();
 const PORT = 8080 || process.env.PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-console.log(__dirname);
+
 /** ★━━━━━━━━━━━★ server httt & websocket ★━━━━━━━━━━━★ */
 
 /** Tenemos dos servidores:  httpServer (http) y io (websocket)*/
@@ -37,6 +43,34 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+
+/** para gestionar cookies dentro de cada endpoint
+ * lo que está entre parentesis es la clave secreta
+ */
+app.use(cookieParser("mySecret"));
+/** guardar session en navegador*/
+// app.use(
+//   session({ secret: "un-re-secreto", resave: true, saveUninitialized: true })
+// );
+/** Persistir session en Mongo Atlas */
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASS = process.env.MONGO_PASS;
+const DB_NAME = process.env.DB_NAME;
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.cyfup.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+      ttl: 60 * 10, // 10 minutes
+    }),
+  })
+);
+app.use(passport.initialize()); // Inicializa passport
+app.use(passport.session()); // Enlaza passport con la sesion
+
+app.use(flash());
 
 /** ★━━━━━━━━━━━★ frontend ★━━━━━━━━━━━★*/
 // Configuración de Express Handlebars
@@ -60,6 +94,20 @@ app.use("/home", homeRoutes);
 app.use("/products", productRoutes);
 app.use("/carts", cartRoutes);
 app.use("/chat", chatRoutes);
+app.use("/auth", authRoutes);
+app.use("/error", (req, res) => {
+  const { errorMessage } = req.flash();
+  res.render("error", { errorMessage });
+});
+
+// redirect to /home
+app.get("/", (req, res) => {
+  res.redirect("/home");
+});
+//not found
+app.use("*", (req, res, next) => {
+  res.render("notfound");
+});
 
 /** ★━━━━━━━━━━━★ connection mongoDB ★━━━━━━━━━━━★ */
 connectMongoDB();
